0e547fad8230325b225b88bacc6b83156c36e51d
jesus <jesusflsa2004@gmail.com>
2025-02-03 15:48:05 -0500

    update: footer ui

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,32 @@
+const { API_URL } = process.env;
+
+import ExternalLink from "@/ui/icons/ExternalLink";
+import GithubIcon from "@/ui/icons/GithubIcon";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="px-6 py-2 text-sm flex justify-between">
-      <p className="text-neutral-500">
-        Proyecto construido por{" "}
+      <p className="text-neutral-500 flex gap-2">
+        Proyecto desarollado por{" "}
         <Link
-          className="underline hover:text-neutral-300"
+          className="text-neutral-400 hover:text-neutral-300 transition-colors flex gap-1 items-center"
           href="https://github.com/jesusflsa"
           target="_blank"
         >
-          jesusflsa
+          <GithubIcon className="w-4 inline-block" />
+          <span>Jesús Flores</span>
         </Link>
-        .
       </p>
       <div>
-        <p>Documentación del API <Link href=""></Link></p>
+        <Link
+          href={`${API_URL}/swagger-ui/index.html`}
+          className="flex gap-1 text-neutral-400 hover:text-neutral-300"
+          target="_blank"
+        >
+          <ExternalLink className="w-4 inline-block" />
+          <span>Documentación del API</span>
+        </Link>
       </div>
     </footer>
   );
